75991bf029d6f7d8d0f7c5d4f26e0625c4bb4d97
Jasmine Ben-Whyte <benwhytejasmine@gmail.com>
2024-04-17 14:35:38 -0400

    creating foundation files

---
+++ Targets/Read.meKit/Tests/Read.meKitTests.swift
@@ -0,0 +1,21 @@
+//
+//  Read.meKitTests.swift
+//  Read.meKitTests
+//
+//  Created by Automated on 17/04/2024.
+//  Copyright © 2024 CompanyName. All rights reserved.
+//
+
+import XCTest
+@testable import Read.meKit
+
+class Read.meKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
